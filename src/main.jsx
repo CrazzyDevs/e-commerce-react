c462f8767370204a441f10f0b6d9ec3881f1ac31
Victor Kipkorir <victorquaint@gmail.com>
2023-06-19 18:25:21 +0300

    Added redirects file and BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import './index.css'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import React from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+);
